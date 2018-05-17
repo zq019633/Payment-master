f472e6b3f7d00b62649fa8c9ce9dd16ceb2be25e
zq019633 <1020498110@qq.com>
2018-05-17 17:39:50 +0800

    修改

--- app/src/main/java/com/example/payment/wxPay/wxUtils/Constants.java
+++ app/src/main/java/com/example/payment/wxPay/wxUtils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     //商户id
     public static final String MCH_ID = "";
     //商户和微信签约的秘钥
-    public static final String API_KEY = "";
+    public static final String API_KEY = "你好";
 
 
 }
